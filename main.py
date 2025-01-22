ef24db8393260d8f5c0557e1fef31d1c63273c6d
cwainright-nps <charles_wainright@nps.gov>
2025-01-22 11:57:30 -0500

    [feature] added workflow examples for utils.water_sites() and utils.wqp_metadata()

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import src.utils as utils
+from arcgis.gis import GIS
+import src.assets as src_assets
 
-if __name__ == 'main':
-    # prod
-    utils.backup_water(verbose=False, test_run=False)
-    utils.backup_veg(verbose=False)
-    utils.dashboard_etl(test_run=False, include_deletes=False, verbose=False) # prod
+target_itemid = src_assets.WATER_PROD_QC_DASHBOARD_BACKEND
+gis = GIS('home') # update to user/pw 
+item = gis.content.get(target_itemid)
+
+utils.backup_water(verbose=True, test_run=True) # test_run=True copies csvs; test_run=False copies csvs and fgdb
+utils.dashboard_etl(test_run=False, include_deletes=False, verbose=True) # prod
+utils.backup_veg(verbose=True)
+
+
+# exports
+dashb = utils.dashboard_etl(test_run=True) # returns a dataframe of un/verified records in dashboard format
+wqp = utils.wqp_wqx(test_run=True) # returns a dataframe of verified records in wqp format
+wqp_metadata = utils.wqp_metadata(test_run=True) # returns a dataframe of metadata to accompany wqp in R package NCRNWater
+water_sites = utils.water_sites(r'output/ncrn_water_locations_20250122.csv') # returns a dataframe of current NCRN water sites and their attributes from AGOL
