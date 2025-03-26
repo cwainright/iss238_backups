ca0ef42df34e8c0a64d325bbdd90d5994ed3e61c
cwainright-nps <charles_wainright@nps.gov>
2025-03-26 11:54:53 -0400

    [bugfix] update flag only when result is missing

--- src/utils.py
+++ src/utils.py
@@ -1180,13 +1180,9 @@ def _wqp_qc(df:pd.DataFrame) -> pd.DataFrame:
     # df['data_quality_flag'] = np.where(mask, 'Not Detected', df['data_quality_flag']) # this is the WQP flag per pg 6 https://www.epa.gov/sites/default/files/2020-08/documents/wqx_web_template_user_guide.pdf
 
     # if the 'sampleability' is 'Unable to Access Site', and the 'data_quality_flag' is NA, update the flag
-    # filter-out the tds rows for <=2024
     # others can be 'not-on_datasheet'
-    cutoff_date = dt.date(2025,1,1)
-    mask = (pd.to_datetime(df['activity_start_date']).dt.date <= cutoff_date) & (df['Characteristic_Name']=='tds') 
-    df = df[~mask] # gets rid of TDS
 
-    mask = (df['sampleability']== 'Unable to Access Site') & (df['data_quality_flag'].isna())
+    mask = (df['sampleability']== 'Unable to Access Site') & (df['data_quality_flag'].isna()) & (df['num_result'].isna())
     df['data_quality_flag'] = np.where(mask, 'not_on_datasheet', df['data_quality_flag']) # update flag for 'Unable to Access Site'
 
 
